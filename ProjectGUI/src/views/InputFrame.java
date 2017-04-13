150262abaf204028928d212ba3ee4606052ed110
neil4droid <cterse@gmail.com>
2017-04-13 22:00:05 +0530

    Input page GUI

--- ProjectGUI/src/views/InputFrame.java
+++ ProjectGUI/src/views/InputFrame.java
@@ -8,10 +8,20 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JLabel;
+import javax.swing.JScrollBar;
 
 public class InputFrame extends JFrame {
 
 	private JPanel contentPane;
+	private JButton btnImportFile;
+	private JButton btnGo;
+	private JButton btnNext;
+	private JTextArea txtrCopyOrImport;
+	private JTextArea textArea;
 
 	/**
 	 * Launch the application.
@@ -33,6 +43,7 @@ public class InputFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public InputFrame() {
+		setTitle("Class Diagram Using NLP");
 		initComponents();
 		createEvents();
 		
@@ -40,23 +51,80 @@ public class InputFrame extends JFrame {
 
 	private void initComponents() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 526, 485);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
+		txtrCopyOrImport = new JTextArea();
+		
+		btnImportFile = new JButton("Import File");
+		
+		JLabel lblInputField = new JLabel("Input Field:");
+		
+		btnGo = new JButton("Go");
+		
+		JLabel lblGeneratedSentences = new JLabel("Generated Sentences:");
+		
+		textArea = new JTextArea();
+		
+		JScrollBar scrollBar = new JScrollBar();
+		
+		JScrollBar scrollBar_1 = new JScrollBar();
+		
+		btnNext = new JButton("Next");
+		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setAutoCreateGaps(true);
-		gl_contentPane.setAutoCreateContainerGaps(true);
-		contentPane.setLayout(gl_contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 424, Short.MAX_VALUE)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblInputField)
+						.addComponent(lblGeneratedSentences)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+								.addComponent(textArea, GroupLayout.DEFAULT_SIZE, 285, Short.MAX_VALUE)
+								.addComponent(txtrCopyOrImport, GroupLayout.DEFAULT_SIZE, 285, Short.MAX_VALUE))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(scrollBar_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(scrollBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+							.addGap(41)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(btnNext, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(btnGo, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(btnImportFile, GroupLayout.DEFAULT_SIZE, 98, Short.MAX_VALUE))))
+					.addGap(43))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 251, Short.MAX_VALUE)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblInputField)
+					.addGap(14)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(scrollBar_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+							.addComponent(txtrCopyOrImport, GroupLayout.DEFAULT_SIZE, 163, Short.MAX_VALUE)
+							.addGroup(gl_contentPane.createSequentialGroup()
+								.addComponent(btnImportFile)
+								.addGap(14)
+								.addComponent(btnGo))))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(btnNext)
+						.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+							.addComponent(lblGeneratedSentences)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(scrollBar, GroupLayout.DEFAULT_SIZE, 172, Short.MAX_VALUE)
+								.addComponent(textArea, GroupLayout.DEFAULT_SIZE, 172, Short.MAX_VALUE))))
+					.addGap(19))
 		);
+		gl_contentPane.setAutoCreateGaps(true);
+		gl_contentPane.setAutoCreateContainerGaps(true);
+		contentPane.setLayout(gl_contentPane);
 		
 	}
 
@@ -64,5 +132,4 @@ public class InputFrame extends JFrame {
 		// TODO Auto-generated method stub
 		
 	}
-
 }
