006cc5148d5c8fcf62548f9b99a7d68b83ffe1ad
neil4droid <cterse@gmail.com>
2017-04-13 18:32:34 +0530

    Started project GUI

---
+++ ProjectGUI/src/views/InputFrame.java
@@ -0,0 +1,68 @@
+package views;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+
+public class InputFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InputFrame frame = new InputFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public InputFrame() {
+		initComponents();
+		createEvents();
+		
+	}
+
+	private void initComponents() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setAutoCreateGaps(true);
+		gl_contentPane.setAutoCreateContainerGaps(true);
+		contentPane.setLayout(gl_contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 424, Short.MAX_VALUE)
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 251, Short.MAX_VALUE)
+		);
+		
+	}
+
+	private void createEvents() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
