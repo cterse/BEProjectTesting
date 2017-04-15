eac0963239779c0718674b1bef571f49cc0b3f84
neil4droid <cterse@gmail.com>
2017-04-15 10:23:49 +0530

    blank commit

---
+++ ProjectGUI/src/views/InputFrame2.java
@@ -0,0 +1,58 @@
+package views;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JScrollPane;
+import java.awt.Rectangle;
+
+public class InputFrame2 extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InputFrame2 frame = new InputFrame2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public InputFrame2() {
+		initComponents();
+		createEvents();
+	}
+
+	private void initComponents() {
+		// TODO Auto-generated method stub
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+	}
+
+	private void createEvents() {
+		// TODO Auto-generated method stub
+		
+	}
+}
